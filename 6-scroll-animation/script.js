0ecc11b5b58d99f178531f368f9965c37504365b
Ibrahima Diagne Seck <120041057+ibrahimadiagneseck@users.noreply.github.com>
2023-06-01 06:23:15 +0000

    commit

--- 6-scroll-animation/script.js
+++ 6-scroll-animation/script.js
@@ -2,7 +2,7 @@ const boxes = document.querySelectorAll('.box')
 
 window.addEventListener('scroll', checkBoxes)
 
-checkBoxes()
+checkBoxes() // initialiser les premiers boxes
 
 function checkBoxes() {
     const triggerBottom = window.innerHeight / 5 * 4
